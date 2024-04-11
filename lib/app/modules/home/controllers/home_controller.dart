18b601639635878e8b87e087b7dfdd92692af921
vepraaa <a.veprikovaa@yandex.ru>
2024-04-11 22:16:07 +0500

    vers1

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,6 +1,26 @@
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
+  // Список категорий
+  final List<String> categories = [
+    'Новинки',
+    'Популярное',
+    'Напитки',
+    'Супер Бокс',
+    'Комбо Обед',
+    'Бургеры и роллы',
+    'Картофель, стартеры и салаты',
+    'Завтрак',
+    'Кидз Комбо',
+    'Десерты',
+    'Соусы и другое',
+  ];
 
-  
+  // Выбранная категория
+  RxString selectedCategory = RxString('');
+
+  // Метод для обновления выбранной категории
+  void setSelectedCategory(String category) {
+    selectedCategory.value = category;
+  }
 }
